2d6b94b2fefbc77bf2ebe5a819322d470f598a22
Praveen Shrivas <47313992+praveenshrivas@users.noreply.github.com>
2025-10-27 16:37:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0912f71e06545ad88"
+  key_name = "AWS-Class"
   instance_type = "t2.micro"
   tags = {
     Name = "EC2-KickOff-Jenkins"
